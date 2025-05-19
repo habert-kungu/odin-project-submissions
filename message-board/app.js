b77170c2e7de757a60108df15b5ed6559b9af947
Habert Kungu <habert231@gmail.com>
2025-05-19 23:00:19 +0300

    Adding  express router: Added route and ejs for templste generation

---
+++ message-board/app.js
@@ -0,0 +1,257 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  },
+  {
+    text: "How's everyone doing today?",
+    user: "Sarah",
+    added: new Date()
+  },
+  {
+    text: "Working on a new project!",
+    user: "Mike",
+    added: new Date()
+  },
+  {
+    text: "Just joined the chat!",
+    user: "Jessica",
+    added: new Date()
+  },
+  {
+    text: "Anyone up for lunch later?",
+    user: "David",
+    added: new Date()
+  },
+  {
+    text: "The weather is beautiful today",
+    user: "Emily",
+    added: new Date()
+  },
+  {
+    text: "Code review at 2pm",
+    user: "Robert",
+    added: new Date()
+  },
+  {
+    text: "Happy Friday everyone!",
+    user: "Lisa",
+    added: new Date()
+  },
+  {
+    text: "Need help with JavaScript",
+    user: "Thomas",
+    added: new Date()
+  },
+  {
+    text: "Just deployed the new feature",
+    user: "Jennifer",
+    added: new Date()
+  },
+  {
+    text: "Coffee break?",
+    user: "Kevin",
+    added: new Date()
+  },
+  {
+    text: "Meeting in 5 minutes",
+    user: "Amanda",
+    added: new Date()
+  },
+  {
+    text: "Check out this cool article!",
+    user: "Brian",
+    added: new Date()
+  },
+  {
+    text: "Who's working from home today?",
+    user: "Nicole",
+    added: new Date()
+  },
+  {
+    text: "I'll be out of office tomorrow",
+    user: "Steven",
+    added: new Date()
+  },
+  {
+    text: "New intern starting today",
+    user: "Michelle",
+    added: new Date()
+  },
+  {
+    text: "Server maintenance tonight",
+    user: "Jason",
+    added: new Date()
+  },
+  {
+    text: "Don't forget to submit your reports",
+    user: "Melissa",
+    added: new Date()
+  },
+  {
+    text: "Weekend plans anyone?",
+    user: "Eric",
+    added: new Date()
+  },
+  {
+    text: "Just fixed that annoying bug",
+    user: "Rachel",
+    added: new Date()
+  },
+  {
+    text: "Who's bringing donuts tomorrow?",
+    user: "Daniel",
+    added: new Date()
+  },
+  {
+    text: "The new update looks great!",
+    user: "Kimberly",
+    added: new Date()
+  },
+  {
+    text: "Reminder: Team building event Friday",
+    user: "Matthew",
+    added: new Date()
+  },
+  {
+    text: "Working remotely today",
+    user: "Laura",
+    added: new Date()
+  },
+  {
+    text: "Just finished my coffee",
+    user: "Andrew",
+    added: new Date()
+  },
+  {
+    text: "Need help with CSS",
+    user: "Stephanie",
+    added: new Date()
+  },
+  {
+    text: "The WiFi is acting up again",
+    user: "Ryan",
+    added: new Date()
+  },
+  {
+    text: "Who's in for after-work drinks?",
+    user: "Olivia",
+    added: new Date()
+  },
+  {
+    text: "New company policy announced",
+    user: "Justin",
+    added: new Date()
+  },
+  {
+    text: "Happy birthday to me!",
+    user: "Christina",
+    added: new Date()
+  },
+  {
+    text: "Vacation starts tomorrow!",
+    user: "Brandon",
+    added: new Date()
+  },
+  {
+    text: "Just got a new desk",
+    user: "Rebecca",
+    added: new Date()
+  },
+  {
+    text: "Who took my charger?",
+    user: "Nathan",
+    added: new Date()
+  },
+  {
+    text: "Lunch in the break room",
+    user: "Samantha",
+    added: new Date()
+  },
+  {
+    text: "The printer is jammed again",
+    user: "Timothy",
+    added: new Date()
+  },
+  {
+    text: "New project kickoff at 3",
+    user: "Heather",
+    added: new Date()
+  },
+  {
+    text: "Code pushed to production",
+    user: "Patrick",
+    added: new Date()
+  },
+  {
+    text: "Who's got the conference room key?",
+    user: "Amy",
+    added: new Date()
+  },
+  {
+    text: "The coffee machine is broken",
+    user: "Gregory",
+    added: new Date()
+  },
+  {
+    text: "Has anyone seen my notebook?",
+    user: "Angela",
+    added: new Date()
+  },
+  {
+    text: "Working late tonight",
+    user: "Scott",
+    added: new Date()
+  },
+  {
+    text: "Traffic was terrible today",
+    user: "Megan",
+    added: new Date()
+  },
+  {
+    text: "New security protocols in place",
+    user: "Benjamin",
+    added: new Date()
+  },
+  {
+    text: "Who's ordering lunch?",
+    user: "Danielle",
+    added: new Date()
+  },
+  {
+    text: "The AC is freezing in here",
+    user: "Zachary",
+    added: new Date()
+  },
+  {
+    text: "Need a break from coding",
+    user: "Tiffany",
+    added: new Date()
+  },
+  {
+    text: "Who's going to the conference?",
+    user: "Samuel",
+    added: new Date()
+  },
+  {
+    text: "My plants need watering",
+    user: "Victoria",
+    added: new Date()
+  },
+  {
+    text: "The new chairs are uncomfortable",
+    user: "Dylan",
+    added: new Date()
+  },
+  {
+    text: "Has anyone tried the new restaurant?",
+    user: "Alexis",
+    added: new Date()
+  },
+ 
